617fb91f066af64f1e78215e7c4dbcde9eb93c34
Dinechko <m@dichenko.ru>
2022-05-24 06:50:21 +0300

    tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,39 +11,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 // https://doc.aurora.dev/interact/hardhat/
-// npx hardhat createСampaign --contractaddr 0x5FbDB2315678afecb367f032d93F642f64180aa3 --addresslist '0x70997970C51812dc3A010C7d01b50e0d17dc79C8, 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC' --bid 1000000000000000000 --duration 3600 --network localhost
-0x5FbDB2315678afecb367f032d93F642f64180aa3 undefined 1000000000000000000 3600
+
+
 task("createСampaign", "Create new campaign").addParam('contractaddr', "Conrtact address")
-  .addParam("addresslist", "Array of the candidates addresses")
-  .addParam("bid", "Bid for voting (in wei)").addParam("duration", "duration of campaign in seconds")
+  .addParam("addresslist", "Array of the candidates addresses").addParam("bid", "Bid for voting (in wei)").addParam("duration", "duration of campaign in seconds")
   .setAction(async ({contractaddr, addressList, bid, duration}, { ethers: { getSigners }, runsuper }) => {
-    console.log(contractaddr, addressList,bid,duration );
     const Votings = await ethers.getContractFactory("Votings");
-    console.log("-1");
     const myContract = Votings.attach(contractaddr);
-    console.log("--2");
-    const [user0, user1] = await ethers.getSigners();
-    console.log("---3");
+    const [user0] = await ethers.getSigners();
     const id = await myContract.connect(user0).createCampaign(addressList, bid, duration);
     console.log("Campaign created with id ${id}");
     return id;
 });
 
 task("vote", "vote").addParam('contractaddr', "Conrtact address")
-  .addParam("campaign", "Campaign index")
-  .addParam("bid", "Bid for voting (in wei)").addParam("duration", "duration of campaign in seconds")
-  .setAction(async ({contractAddr, addressList, bid, duration}, { ethers: { getSigners }, runsuper }) => {
+  .addParam("signerid", "Signer index (0-9)").addParam("campaign", "Campaign index")
+  .addParam("candidate", "Candidate address")
+  .setAction(async ({contractaddr, candidate, campaign, signerid}, { ethers: { getSigners }, runsuper }) => {
     const Votings = await ethers.getContractFactory("Votings");
-    const myContract = Votings.attach(contractAddr);
-    const [user0] = hre.ethers.getSigners();
-    const id = await myContract.connect(user0).createCampaign(taskArgs.addressList, taskArgs.bid, taskArgs.duration);
-    console.log("Campaign created with id ${id}");
+    const myContract = Votings.attach(contractaddr);
+    const [user0, user1, user2, user3, user4, user5, user6, user7, user8, user9] = hre.ethers.getSigners();
+    const userArray = [user0, user1, user2, user3, user4, user5, user6, user7, user8, user9];
+    const [, , , bid, , ,] = await myContract.getCampaignInformation(0);
+    const options = { value: bid };
+    const id = await myContract.connect(userArray[signerid]).vote(campaign, candidate, options);
+    console.log("User ${userArray[signerid]} vote for candidate ${candidate}");
     return id;
 });
 
 task("getcomissionpercent", "Get comission percent").addParam('contractaddr', "Conrtact address")
-.setAction(async ({contractaddr},{ ethers: { getSigners }, runsuper }) => {
-
+.setAction(async ({contractaddr}) => {
+  Votings = await ethers.getContractFactory("Votings");
+  const myContract = Votings.attach(contractaddr);
+  const comissionPercent = await myContract.getComissinPercent;
+  console.log("Comission percent = ${comissionPercent}")
+  return comissionPercent;
 });
 
 
