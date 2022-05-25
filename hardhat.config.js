c191510edc1ec9e374663742ef3912cf2a7b79f8
Dinechko <m@dichenko.ru>
2022-05-25 10:19:15 +0300

    update tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,30 +11,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-
-task("createСampaign", "Create new campaign").addParam('contractaddr', "Conrtact address")
+task("createcampaign", "Create new campaign").addParam('contractaddr', "Conrtact address")
   .addParam("addresslist", "Array of the candidates addresses").addParam("bid", "Bid for voting (in wei)").addParam("duration", "duration of campaign in seconds")
   .setAction(async ({contractaddr, addressList, bid, duration}, { ethers: { getSigners }, runsuper }) => {
     const Votings = await ethers.getContractFactory("Votings");
     const myContract = Votings.attach(contractaddr);
-    const [user0] = await ethers.getSigners();
-    const id = await myContract.connect(user0).createCampaign(addressList, bid, duration);
-    console.log("Campaign created with id ${id}");
+    const user = await ethers.getSigners();
+    const id = await myContract.createCampaign(addressList, bid, duration);
+    console.log("Campaign created with id", id);
     return id;
 });
 
 task("vote", "vote").addParam('contractaddr', "Conrtact address")
-  .addParam("signerid", "Signer index (0-9)").addParam("campaign", "Campaign index")
+  .addParam("campaign", "Campaign index")
   .addParam("candidate", "Candidate address")
   .setAction(async ({contractaddr, candidate, campaign, signerid}, { ethers: { getSigners }, runsuper }) => {
     const Votings = await ethers.getContractFactory("Votings");
     const myContract = Votings.attach(contractaddr);
-    const [user0, user1, user2, user3, user4, user5, user6, user7, user8, user9] = hre.ethers.getSigners();
-    const userArray = [user0, user1, user2, user3, user4, user5, user6, user7, user8, user9];
-    const [, , , bid, , ,] = await myContract.getCampaignInformation(0);
-    const options = { value: bid };
-    const id = await myContract.connect(userArray[signerid]).vote(campaign, candidate, options);
-    console.log("User ${userArray[signerid]} vote for candidate ${candidate}");
+    const user = ethers.getSigners();
+    const [, , , bid, , ,] = await myContract.getCampaignInformation(campaign);
+    const options = { value: Number(bid) };
+    console.log(Number(bid));
+    const id = await myContract.vote(campaign, candidate, options);
+    console.log("You vote for candidate", candidate);
     return id;
 });
 
